733b55c3090707a119d5fd17afdf99a545989f37
Anton3311 <antonplakhotia@gmail.com>
2025-05-11 11:32:41 +0300

    Added log4j & sqlite dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
+    implementation("org.xerial:sqlite-jdbc:3.49.1.0")
 }
 
 tasks.test {
