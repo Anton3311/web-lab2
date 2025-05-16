9234711e8cdb889717cc197e41ace74ebf69a4ed
Anton3311 <antonplakhotia@gmail.com>
2025-05-16 18:49:19 +0300

    Finally got at least something working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("war")
 }
 
 group = "org.example"
@@ -14,8 +15,14 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.apache.logging.log4j:log4j-core:2.17.2")
     implementation("org.xerial:sqlite-jdbc:3.49.1.0")
+    // https://mvnrepository.com/artifact/javax.servlet/javax.servlet-api
+    compileOnly("javax.servlet:javax.servlet-api:3.0.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.war {
+    webAppDirectory = file("src/main/webapp")
 }
