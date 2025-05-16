37d6d3cc8f893d77a9e7eddc7e6a13a120b32675
Anton3311 <antonplakhotia@gmail.com>
2025-05-16 19:24:51 +0300

    Send a basic response in a `FrontController`

--- src/main/java/org/example/controller/FrontController.java
+++ src/main/java/org/example/controller/FrontController.java
@@ -18,7 +18,9 @@ public class FrontController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("<p>hello</p>");
     }
 
     @Override
